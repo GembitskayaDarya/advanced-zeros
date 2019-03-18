b10aace66057d29fb19574b472fc182e6874a3db
darya.gembitskaya <daxagemb@gmail.com>
2019-03-18 19:55:37 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let ans = Math.pow(10, 9);
+    for(let i= 2; i <= base; i++){
+      if(base % i === 0){
+      let count1 = 0;
+      while(base % i === 0){
+        count1++;
+          base /= i;
+          base = parseInt(base);
+      }
+      let count2 = 0;
+      let t = number;
+      while(t / i > 0){
+          t /= i;
+          t = parseInt(t);
+        count2 += t;
+
+      }
+      ans = Math.min(ans, count2 / count1);
+    }
+  }
+  return parseInt(ans);
 }
